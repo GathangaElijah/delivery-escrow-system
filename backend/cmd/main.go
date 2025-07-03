b04a55a8f2aebf64fa29a78134b82576eda3811b
GathangaElijah <elyg3672@gmail.com>
2025-07-03 20:54:29 +0300

    feat: added a wrapper for the middleware for debugging"

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,45 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os/exec"
-	"runtime"
+	"os"
 
-	"des/backend/api/v1"
+	multiplexer "des/backend/api/v1"
+	middleware "des/backend/middleware"
 )
-
-// openBrowser tries to open the URL in the default browser
-func openBrowser(url string) {
-	var err error
-
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
-
-	if err != nil {
-		log.Printf("Error opening browser: %v", err)
-	}
-}
 
 func main() {
 	mux := multiplexer.Multiplexer()
+	// DEBUG
+	handler := middleware.EnableCORS(mux)
 
 	// Define server address
-	addr := ":8080"
-	url := fmt.Sprintf("http://localhost%s", addr)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":5001"
+	}
+
+	url := fmt.Sprintf("http://localhost%s", port)
 
 	// Print clickable link in terminal
 	fmt.Printf("Server starting at \033[34m\033[4m%s\033[0m\n", url)
 
 	// Start the server
-	log.Printf("Starting server on %s", addr)
-	err := http.ListenAndServe(addr, mux)
+	log.Printf("Starting server on %s", port)
+	err := http.ListenAndServe(port, handler)
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
