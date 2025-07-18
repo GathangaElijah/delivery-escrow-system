34e3ab884dde17f11de92e6fee5f6469c54c9992
GathangaElijah <elyg3672@gmail.com>
2025-07-18 21:03:09 +0300

    refactor: imported the navbar component into the main(root)

--- descrow-frontend/src/main.jsx
+++ descrow-frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/Login.jsx'
+import Login from './components/Login.jsx'
+import NavBar from './components/NavBar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <NavBar />
+    {/* <Login /> */}
   </StrictMode>,
 )
