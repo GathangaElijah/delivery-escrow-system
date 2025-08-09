5d58bb2ff45eacb463f6854e722939bfa48fa95d
GathangaElijah <elyg3672@gmail.com>
2025-08-09 13:35:16 +0300

    feat: connecting wallet to app

--- descrow-frontend/src/App.tsx
+++ descrow-frontend/src/App.tsx
@@ -1,6 +1,10 @@
+import React from 'react'
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
 import { wagmiConfig } from './wagmi.config'
+
+import WalletConnect from './ConnectWallet'
 
 import './App.css'
 
@@ -11,7 +15,10 @@ function App() {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        {/** ... */}
+        <div style={{ padding: '2rem' }}>
+          <h1>My DApp</h1>
+          <WalletConnect />
+        </div>
       </QueryClientProvider>
     </WagmiProvider>
   )
